acca7ac1460036bd8789d88050faaa58cff83315
LiliaIsABell <lilisa_alugo@yahoo.com>
2021-03-27 19:38:02 -0400

    Initial commit

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code from Acacia Developer
+// https://www.youtube.com/watch?v=PmIPqGqp8UY
+
+public class CameraMove : MonoBehaviour
+{
+   public Transform playerCamera = null;
+   public float mouseSensitivity = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      UpdateMouseLook();
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
+    }
+}
---
+++ Assets/Scripts/WallMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMove : MonoBehaviour
+{
+    public Vector3 startPos;
+    public float distance = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         transform.position = startPos + new Vector3(0.0f, Mathf.Sin(Time.time) * distance, 0.0f);
+    }
+}
