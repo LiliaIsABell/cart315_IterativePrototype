8bc219efc0829e848fa6581a66195d65263f93c9
LiliaIsABell <lilisa_alugo@yahoo.com>
2021-04-11 20:55:21 -0400

    RedWall open when standing in front

--- Assets/Scripts/WallMove.cs
+++ Assets/Scripts/WallMove.cs
@@ -4,18 +4,46 @@ using UnityEngine;
 
 public class WallMove : MonoBehaviour
 {
-    public Vector3 startPos;
-    public float distance = 10f;
+  
+    public GameObject wall;
+    public bool nearWall;
     
-    // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position; 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-         transform.position = startPos + new Vector3(0.0f, Mathf.Sin(Time.time) * distance, 0.0f);
+        if (nearWall)
+        {
+            if (wall.activeInHierarchy)
+            {
+                wall.SetActive(false);
+            }
+            else
+            {
+                wall.SetActive(true);
+            }
+
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            nearWall = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            nearWall = false;
+            wall.SetActive(false);
+        }
     }
 }
